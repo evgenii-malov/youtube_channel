19be1ddd343fd4517e1f249665a1f039fec19fa3
evgenii-malov <evgeniiml@gmail.com>
2020-10-30 13:39:34 +0300

    Update main.py

--- python/number_factorization/main.py
+++ python/number_factorization/main.py
@@ -1,2 +1,2 @@
-# https://www.youtube.com/watch?v=mq0nCzeWlYs&t=96s
+# explanation - https://www.youtube.com/watch?v=mq0nCzeWlYs&t=96s
 ff = lambda n:(p:=[next(i for i in range(2,n+1) if n % i ==0)] if n>1 else [])+(ff(n//p[0]) if p else [])
