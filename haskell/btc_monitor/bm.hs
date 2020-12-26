7064ffa55c238a7042dd5cfb56892d8f9dbb1e08
evgenii-malov <evgeniiml@gmail.com>
2020-12-26 13:07:09 +0300

    Update bm.hs

--- haskell/btc_monitor/bm.hs
+++ haskell/btc_monitor/bm.hs
@@ -7,19 +7,13 @@ import Control.Exception
 import Control.Applicative
 
 u = "https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC"
-
 print_or_not pp p = if pp /= p then print p else return ()
-
 parsed d = head $ (jPath "result/Ask" d) <|> ["parse_price_error"]
-
-
 act pp = (unpack <$> (simpleHttp u))
       >>= (\d -> print_or_not pp (parsed d)
                  >> threadDelay 1000000
                  >> act (parsed d)
       )
-
 onerr :: String -> HttpException -> IO ()
 onerr pp e = (print $ "error: " ++ show e) >> threadDelay 1000000 >> go pp
-
 go pp = catch (act pp) (onerr pp)
